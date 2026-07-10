09d7a1ea08ba8c24066bbf2434a81d163ad0161a
Kittipat Srilekarat <78522320+TONMAIIII@users.noreply.github.com>
2026-07-10 09:55:15 +0700

    1

---
+++ week1/lab1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+int main(){
+    printf("Hello, World!\n\n");
+    printf("%s \n", "69011373 Kittipat Sr.");
+    printf("%d\n", 100);
+    printf("%f\n", 3.14);
+    printf("%c\n", 'A');
+
+    printf("%s %d %f %c\n", "Hello", 100, 3.14, 'A');
+
+
+    return 0;
+}
+
