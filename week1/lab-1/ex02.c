33a29edafc31d0edd0502c802e6a0663320d4249
Kittipat Srilekarat <78522320+TONMAIIII@users.noreply.github.com>
2026-07-10 11:04:32 +0700

    r

---
+++ week1/lab-1/ex02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("%s\n", "My name is PoomK");
+    printf("I am %d years old\n", 99);
+    printf("I am %.1f cm tall\n", 160.5);
+
+
+    return 0;
+}
---
+++ week1/lab-1/ex07.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n");
+    printf("\t^\t^\n");
+    printf("(\t.\t.\t)\n");
+    printf("\t\t-\n");
+
+    return 0;
+}
