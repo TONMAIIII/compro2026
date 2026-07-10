ae9f6247a4f17f336008cd8abc77211b2c2d0c28
Kittipat Srilekarat <78522320+TONMAIIII@users.noreply.github.com>
2026-07-10 11:09:43 +0700

    3

---
+++ week1/prac3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter the first value: ");
+    scanf("%d", &a);
+    printf("Enter the second value: ");
+    scanf("%d", &b);
+    printf("%d + %d = %d", a, b, a + b);
+
+    return 0;
+}
