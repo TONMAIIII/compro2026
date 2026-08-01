7d3dae61bfd925f42333179cf68e5d8d4aca0e9a
Kittipat Srilekarat <78522320+TONMAIIII@users.noreply.github.com>
2026-08-01 10:27:05 +0700

    asd

---
+++ week4/lab/ex01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int num[10];
+    for(int i = 0; i < 10; i++){
+        printf("%d. Enter a number: ", i + 1);
+        scanf("%d", &num[i]);
+    }
+    printf("total sum is %d\n", num[0] + num[1] + num[2] + num[3] + num[4] + num[5] + num[6] + num[7] + num[8] + num[9]);
+    
+    printf("Average is %.02f\n", (float)(num[0] + num[1] + num[2] + num[3] + num[4] + num[5] + num[6] + num[7] + num[8] + num[9]) / 10);
+
+    return 0;
+}
