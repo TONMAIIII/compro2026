17e876bf9862ef35756fd61336c2f69040de6a38
Kittipat Srilekarat <78522320+TONMAIIII@users.noreply.github.com>
2026-08-07 11:00:51 +0700

    ra

--- week4/lab/ex03.c
+++ week4/lab/ex03.c
@@ -2,13 +2,13 @@
 
 int main(){
     int a;
-    while(1){
+    do{
         printf("Enter a number: ");
         scanf("%d", &a);
         if(a == 0) break;
         if(a % 2 == 0)printf("%d is even\n",a);
         else printf("%d is odd\n",a);
-    }
+    } while(1);
     printf("Exiting Program... Bye\n");
     return 0;
 }
